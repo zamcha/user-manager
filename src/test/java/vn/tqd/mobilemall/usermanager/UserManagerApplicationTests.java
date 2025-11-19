209def11656e7ef4e522fa18ce34400c8e7f5b17
zamcha <caotran293@gmail.com>
2025-11-19 20:25:02 +0700

    Innitilation Fisrt

---
+++ src/test/java/vn/tqd/mobilemall/usermanager/UserManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.tqd.mobilemall.usermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
